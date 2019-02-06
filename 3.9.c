411ad73de6cf2ecd039199330d42116e37f37e17
Suifeng0214 <47234216+Suifeng0214@users.noreply.github.com>
2019-02-07 01:20:23 +0800

    ..

---
+++ 3.9.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+main()
+{
+int a,b,c,x,y;
+scanf("%d\n",&a);
+scanf("%d\n",&b);
+scanf("%d",&c);
+ x = 2*(a*b+b*c+c*a) ;
+ y = a*b*c;
+ printf("%d\n",x);
+  printf("%d\n",y);
+}
