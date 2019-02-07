b66d5f50ef5a75b7f724ed150844f69ce88e66fc
Suifeng0214 <47234216+Suifeng0214@users.noreply.github.com>
2019-02-07 13:44:12 +0800

    ..

--- 3.10.c
+++ 3.10.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 main()
 {
-int a, b, c, d, e, x;
-scanf("%d\n",&a);
-scanf("%d\n",&b);
-scanf("%d\n",&c);
-scanf("%d\n",&d);
-scanf("%d",&e);
- x = 2*(a*b+b*c+c*a) + ((a-2*e)*d*8) + ((b-2*e)*d*8) + ((c-2*e)*d*8);
- printf("%d\n",x);
+    int a, b, c, d, e, x;
+
+    scanf("%d\n",&a);
+    scanf("%d\n",&b);
+    scanf("%d\n",&c);
+    scanf("%d\n",&d);
+    scanf("%d",&e);
+
+    x = 2*(a*b+b*c+c*a) + ((a-2*e)*d*8) + ((b-2*e)*d*8) + ((c-2*e)*d*8);
+    printf("%d\n",x);
 }
--- 3.9.c
+++ 3.9.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 main()
 {
-int a, b, c;
-int x, y;
-scanf("%d\n",&a);
-scanf("%d\n",&b);
-scanf("%d",&c);
- x = 2*(a*b+b*c+c*a) ;
- y = a*b*c;
- printf("%d\n",x);
- printf("%d\n",y);
+    int a, b, c;
+    int x, y;
+
+    scanf("%d\n",&a);
+    scanf("%d\n",&b);
+    scanf("%d",&c);
+
+    x = 2*(a*b+b*c+c*a) ;
+    y = a*b*c;
+    printf("%d\n",x);
+    printf("%d\n",y);
 }
