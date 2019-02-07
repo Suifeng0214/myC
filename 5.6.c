a0ea2be5206d916109f05f0bb4b385f325c2a2dd
Suifeng0214 <47234216+Suifeng0214@users.noreply.github.com>
2019-02-07 23:03:41 +0800

    ..

--- 5.6.c
+++ 5.6.c
@@ -1,47 +1,71 @@
 #include <stdio.h>
 main()
 {
-    int year2, month2 ,day2, i, tempday;
+    int year2, month2 ,day2, tempday;
     int year=1970, month=1, day=1;
 
     scanf("%d",&year2);
     scanf("%d",&month2);
     scanf("%d",&day2);
 
-    for (i=1;i<0;i++){
-        day++;
+    for (;;){
         if (year == year2 && month == month2 && day == day2){
-            printf("%d",tempday);
+            printf("%d",tempday-1);
         }
+        day ++;
+        tempday ++;
         switch (month){
             case 1: case 3: case 5: case 7: case 8: case 10: case 12:
-                if (day == 30){
-                    tempday += 30;
+                if (day > 31){
                     day = 1;
                     if (month < 13){
-                    month ++;
+                        month ++;
                     }
                     else{
-                    month = 1;
-                    year ++;
+                        month = 1;
+                        year ++;
                     }
                 }
             break;
             case 4: case 6: case 9: case 11:
-                if (day == 31){
-                    tempday += 31;
+                if (day > 30){
                     day = 1;
                     if (month < 13){
-                    month ++;
+                        month ++;
                     }
                     else{
-                    month = 1;
-                    year ++;
+                        month = 1;
+                        year ++;
                     }
                 }
-                break;
-            default:
-                month = 0;
+            break;
+            case 2:
+                if (year % 400 == 0 ||
+                    year % 4 == 0 && year % 100 != 0){
+                    if (day > 29){
+                        day = 1;
+                        if (month < 13){
+                            month ++;
+                        }
+                        else{
+                            month = 1;
+                            year ++;
+                        }
+                    }
+                }
+                else{
+                    if (day > 28){
+                        day = 1;
+                        if (month < 13){
+                            month ++;
+                        }
+                        else{
+                            month = 1;
+                            year ++;
+                    }
+                    }
+                }
+            break;
         }
     }
 }
