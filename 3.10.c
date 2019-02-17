07bd62464ee217dfd6b6c80bbc980eff16c883e4
Suifeng0214 <47234216+Suifeng0214@users.noreply.github.com>
2019-02-18 01:40:15 +0800

    ..

--- 3.10.c
+++ 3.10.c
@@ -3,10 +3,10 @@ main()
 {
     int a, b, c, d, e, x;
 
-    scanf("%d\n",&a);
-    scanf("%d\n",&b);
-    scanf("%d\n",&c);
-    scanf("%d\n",&d);
+    scanf("%d",&a);
+    scanf("%d",&b);
+    scanf("%d",&c);
+    scanf("%d",&d);
     scanf("%d",&e);
 
     x = 2*(a*b+b*c+c*a) + ((a-2*e)*d*8) + ((b-2*e)*d*8) + ((c-2*e)*d*8);
--- 3.9.c
+++ 3.9.c
@@ -4,8 +4,8 @@ main()
     int a, b, c;
     int x, y;
 
-    scanf("%d\n",&a);
-    scanf("%d\n",&b);
+    scanf("%d",&a);
+    scanf("%d",&b);
     scanf("%d",&c);
 
     x = 2*(a*b+b*c+c*a) ;
