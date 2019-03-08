efd7e58faae91e44deb6548563d50f9db6125458
Suifeng0214 <47234216+Suifeng0214@users.noreply.github.com>
2019-03-08 19:09:12 +0800

    ..

---
+++ 5.8.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+main()
+{
+    int formin, formout;
+    int digit[digits], digits;
+    int temp;
+
+    scanf("%d",&formin);
+    scanf("%d",&digits);
+    for (i=0;i<digits;i++){
+        scanf("%d",digit[i])
+        }
+    scanf("%d",&formout);
+
+
+    if (formin == formout){
+        for (i=0;i<digits;i++){
+            printf("%d\n",digit[i])
+        }
+    else{
+       switch (formin){
+
+       }
+    }
+    }
+}
