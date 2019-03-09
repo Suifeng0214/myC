f14ceb0617fb0e43bfc3077d79e9be6aac969e84
Suifeng0214 <47234216+Suifeng0214@users.noreply.github.com>
2019-03-09 14:11:20 +0800

    ..

--- 5.8.c
+++ 5.8.c
@@ -1,26 +1,66 @@
 #include <stdio.h>
 main()
 {
-    int formin, formout;
-    int digit[digits], digits;
-    int temp;
+    int formin, formout, forminX = 1;
+    int digits,tenthdg;
+    int digit[digits];
+    int temp, i;
 
     scanf("%d",&formin);
     scanf("%d",&digits);
     for (i=0;i<digits;i++){
-        scanf("%d",digit[i])
-        }
+        scanf("%d",&digit[i]);
+    }
     scanf("%d",&formout);
+
+    temp = formin;
+    for (i=0;i<digits;i++)(
+        temp = formin;
+        temp *=
+    )
+
+    for (i=0;i<digits;i++)(
+    tenthdg +=  digit[i]*formin[j]
+    )
+
+
+
+
+
 
 
     if (formin == formout){
         for (i=0;i<digits;i++){
-            printf("%d\n",digit[i])
+            printf("%d\n",digit[i]);
         }
-    else{
-       switch (formin){
-
-       }
     }
+    else{
+        switch (formin){
+            case 2:
+                switch (formout){
+                    case 8:
+
+                    case 10:
+
+                break;
+                }
+            case 8:
+                switch (formout){
+                    case 2:
+
+                    case 10:
+
+                break;
+                }
+            case 10:
+                switch (formout){
+                    case 2:
+
+                    case 8:
+
+                break;
+                }
+        break;
+        }
     }
 }
