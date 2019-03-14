8207b39943ce6be272c3d38eca2596482fd4739b
Suifeng0214 <47234216+Suifeng0214@users.noreply.github.com>
2019-03-15 05:27:11 +0800

    ..

--- 5.8.c
+++ 5.8.c
@@ -2,30 +2,30 @@
 main()
 {
     int formin, formout;
-    int digits,tenthdg=0, tempDG;
-    int temp, i, x=1;
+    int digits, tenthdg = 0, tempDG;
+    int temp, i, x = 1 ;
     scanf("%d",&formin);
     scanf("%d",&digits);
     int digit[digits];
     int temp2[digits];
 
-    for (i=0;i<digits;i++){
+    for (i=0; i < digits; i++){
         scanf("%d",&digit[i]);
     }
     scanf("%d",&formout);
 
     temp = formin;
-    for (i=0;i<digits-2;i++){
+    for (i=0; i < (digits-2); i++){
         temp *= formin;
     }
 
     temp2[0] = temp;
-    for (i=0;i<digits-1;i++){
+    for (i=0; i < (digits-1); i++){
         temp2[i+1] = temp2[i]/formin;
     }
 
-    for (i=0;i<digits;i++){
-    tenthdg +=  digit[i]*temp2[i];
+    for (i=0; i < digits; i++){
+    tenthdg +=  digit[i] * temp2[i];
     }
     tempDG = tenthdg;
 
@@ -40,18 +40,18 @@ main()
 
     int out[x+1];
 
-    for (i=x;i>=0;i--){
+    for (i = x; i >= 0; i--){
         out[i] = tempDG % formout;
         tempDG /= formout;
     }
 
     if (formin == formout){
-        for (i=0;i<digits;i++){
+        for (i = 0; i < digits; i++){
             printf("%d\n",digit[i]);
         }
     }
     else{
-        for (i=0;i<x+1;i++){
+        for (i = 0; i < (x+1); i++){
             printf("%d\n",out[i]);
         }
     }
