ff22ee153cb414101f543b68d7e43cb17409f76a
Suifeng0214 <47234216+Suifeng0214@users.noreply.github.com>
2019-04-03 14:09:49 +0800

    ..

--- 6.2.c
+++ 6.2.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 main()
 {
-    int i, j, lengh;
+    int i, j, k, lengh;
     scanf("%d",&lengh);
     int numbers[lengh];
 
     for (i = 0; i < lengh; i++){
         scanf("%d",&numbers[i]);
     }
-    for (j = lengh - 1; j >= 0; j --){
+    for (j = 0; j < lengh; j += 2){
         printf("%d ",numbers[j]);
+    }
+    printf("\n");
+    for (k = 1; k < lengh; k += 2){
+        printf("%d ",numbers[k]);
     }
 }
