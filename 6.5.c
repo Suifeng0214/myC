9884fa68f03f22ac0208fe1663a00d2a1778fa1b
Suifeng0214 <47234216+Suifeng0214@users.noreply.github.com>
2019-04-22 19:53:20 +0800

    ..

--- 6.5.c
+++ 6.5.c
@@ -7,10 +7,6 @@ main()
     scanf("%d%d", &studentNum, &subjectNum);
     int Grades[studentNum][subjectNum];
     int AVG[studentNum + subjectNum];
-    
-    for (i = 0; i < studentNum + subjectNum;i++){
-    	AVG[i] = 0;
-	}
 	
     for (i = 0; i < studentNum; i++){
 		for (j = 0; j < subjectNum; j++){
@@ -19,21 +15,5 @@ main()
     }
     
 	printf("\n");
-	//=-=-=-=-=-=-=-=
-    for (i = 0; i < studentNum; i++){
-		for (j = 0; j < subjectNum; j++){
-			AVG[i] += Grades[i][j];
-    	}
-    	AVG[i] /= subjectNum;
-    }
-    for (i = studentNum; i < studentNum + subjectNum; i++){
-		for (j = 0; j < studentNum; j++){
-			AVG[i] += Grades[j][i-studentNum];
-    	}
-    	AVG[i] /= studentNum;
-    }
-    //=-=-=-=-=-=-=-=
-    for (i = 0; i < studentNum + subjectNum;i++){
-    	printf("%d\n",AVG[i]);
-	}
+	  
 }
